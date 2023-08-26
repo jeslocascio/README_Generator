1ac971fa7cbf80f4e2e5259823ba8b732e492498
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-08-26 17:07:00 -0400

    Added function to generate Readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,20 +2,52 @@
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
 
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+ ## License
+  This project is licensed under the terms of the **[${
+    data.license
+  }](${renderLicenseLink(data.license)})**
+  
+  ## Contribution
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions or suggestions about this project, please feel free to contact me:
+    GitHub: @${data.github}
+    Email: ${data.email}
 `;
 }
 
